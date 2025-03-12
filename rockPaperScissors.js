07896ca2569f8b88a49329ad6c48245f616ba497
shkhkmt <93771404+shkhkmt@users.noreply.github.com>
2025-03-12 23:48:41 +0200

    added event listener for buttons on UI, and included extra div to contain results through rounds. Need to appened results of rounds to UI

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -13,7 +13,7 @@ header.textContent = "Rock, Paper, Scissors";
 content.insertBefore(header, rules); 
 
 let winCond = document.createElement('p'); 
-winCond.textContent = "First player to five wins!"; 
+winCond.textContent = "First player to five wins."
 rules.appendChild(winCond); 
 
 let weapon = document.createElement('p'); 
@@ -22,52 +22,83 @@ rules.appendChild(weapon);
 
 let human = document.createElement('div'); 
 human.id = 'human'; 
-let hrck = document.createElement('div'); 
-hrck.id = "hrck"; 
-let hppr = document.createElement('div'); 
-hppr.id = "hppr"; 
-let hscrs = document.createElement('div'); 
-hscrs.id = "hscrs"; 
+let humanIcon = document.createElement('div'); 
+humanIcon.id = 'humanIcon'
+let humanActions = document.createElement('div'); 
+humanActions.id = 'humanActions'; 
 
 let robot = document.createElement('div'); 
-robot.id = "robot"; 
-let rrck = document.createElement('div'); 
-rrck.id = "rrck";
-let rppr = document.createElement('div'); 
-rppr.id = "rppr"; 
-let rscrs = document.createElement('div'); 
-rscrs.id = "rscrs"; 
+robot.id = 'robot'; 
+let robotIcon = document.createElement('div');
+robotIcon.id = 'robotIcon'; 
+let robotActions = document.createElement('div'); 
+robotActions.id = 'robotActions';
+let hmnScr = document.createElement('p'); 
+hmnScr.id = 'hmnScr'; 
+hmnScr.textContent = `Human Score:`
 
-game.appendChild(human); 
-human.appendChild(hrck);
-human.appendChild(hppr); 
-human.appendChild(hscrs); 
+let resultsHuman = document.createElement('div'); 
+resultsHuman.id = 'hmnRslt'; 
+let resultsRobot = document.createElement('div');
+resultsRobot.id = 'rbtRslt'; 
+let rbtScr = document.createElement('p');
+rbtScr.id = 'rbtScr'; 
+rbtScr.textContent = 'Robot Score:' 
+
+let footer = document.createElement('footer'); 
+footer.id = 'footer'; 
+footer.textContent = 'Copyright © 2025 shkhkmt'; 
+
+game.appendChild(human);
+human.appendChild(humanIcon);
+human.appendChild(resultsHuman);
+resultsHuman.appendChild(hmnScr); 
+human.appendChild(humanActions);
 
 game.appendChild(robot); 
-robot.appendChild(rrck); 
-robot.appendChild(rppr); 
-robot.appendChild(rscrs); 
+robot.appendChild(robotIcon);
+robot.appendChild(resultsRobot);
+resultsRobot.appendChild(rbtScr); 
+robot.appendChild(robotActions); 
 
-let btnRock = document.createElement('button'); 
-let btnPaper = document.createElement('button'); 
-let btnScissors = document.createElement('button'); 
+content.appendChild(footer); 
 
 let imgRck = document.createElement('img'); 
 let imgPpr = document.createElement('img'); 
 let imgScrs = document.createElement('img'); 
+let imgHmn = document.createElement('img'); 
+let imgRbt = document.createElement('img')
+
+let btnRock = document.createElement('button');
+btnRock.id = 'rckBtn'; 
+let btnPaper = document.createElement('button'); 
+btnPaper.id = 'pprBtn'; 
+let btnScissors = document.createElement('button'); 
+btnScissors.id = 'scrsBtn'; 
 
 imgRck.src = './img/rock-svgrepo-com.svg';
 imgRck.alt = 'Rock'; 
+imgRck.id = 'imgRck';  
 
 imgPpr.src = './img/paper-svgrepo-com.svg'; 
-imgPpr.alt = 'Paper'; 
+imgPpr.alt = 'Paper';
+imgPpr.id = 'imgPpr';  
 
 imgScrs.src = './img/scissors-svgrepo-com.svg'; 
-imgScrs.alt = 'Scissors'; 
+imgScrs.alt = 'Scissors';
+imgScrs.id = 'imgScrs'; 
 
-hrck.appendChild(btnRock); 
-hppr.appendChild(btnPaper); 
-hscrs.appendChild(btnScissors);
+imgHmn.src = './img/human-handsup-svgrepo-com.svg'
+imgHmn.alt = 'human icon'; 
+
+imgRbt.src = './img/robot-svgrepo-com (1).svg';
+imgRbt.alt = 'robot icon'; 
+
+humanIcon.appendChild(imgHmn); 
+robotIcon.appendChild(imgRbt); 
+humanActions.appendChild(btnRock); 
+humanActions.appendChild(btnPaper); 
+humanActions.appendChild(btnScissors);
 
 btnRock.appendChild(imgRck); 
 btnPaper.appendChild(imgPpr); 
@@ -77,38 +108,43 @@ let robotRock = btnRock.cloneNode(true);
 let robotPaper = btnPaper.cloneNode(true); 
 let robotScissors = btnScissors.cloneNode(true); 
 
-document.getElementById('rrck').appendChild(robotRock); 
-document.getElementById('rppr').appendChild(robotPaper); 
-document.getElementById('rscrs').appendChild(robotScissors); 
+document.getElementById('robotActions').appendChild(robotRock); 
+document.getElementById('robotActions').appendChild(robotPaper); 
+document.getElementById('robotActions').appendChild(robotScissors);
 
-/*
+
 function getComputerChoice() {
     let choice = Math.random();
     if (choice <= (1/3)) { // this can be an arrow function
         return 'rock';
     } else if (choice >= (2/3)) {
-    return 'paper';
+        return 'paper';
     } else {
-    return 'scissors';
+        return 'scissors';
     }
 }
 
-function getHumanChoice() {
-    let choice = String(prompt("Rock, Paper or Scissors..? ").toLowerCase());
-    if  (choice == 'rock'){
-        return String(choice)
-    } else if (choice == 'paper') {
-        return String(choice)
-    } else if (choice == 'scissors') {
-        return String(choice)
-    } else {
-        return 'Please try again';
-    }
-}
-// setting global variables for functions.
+let computerChoice = getComputerChoice(); 
+let humanChoice; 
 
-const humanChoice = String(getHumanChoice());
-const computerChoice = String(getComputerChoice());
+humanActions.addEventListener('click', (event) => {
+  let target = event.target; 
+
+  switch(target.id) {
+    case 'imgRck': 
+      humanChoice = 'rock';
+      break; 
+    case 'imgPpr':
+      humanChoice = 'paper'; 
+      break; 
+    case 'imgScrs': 
+      humanChoice = 'scissors'; 
+      break; 
+    default:
+      return null; 
+  }
+});
+
 let humanScore = 0;
 let computerScore = 0;
 
@@ -120,9 +156,10 @@ function playRound(humanChoice, computerChoice) {
 
     if ((humanChoice == 'rock'  && computerChoice == 'scissors') ||
         (humanChoice == 'scissors' && computerChoice == 'paper') ||
-        (humanChoice == 'paper' && computerChoice == 'rock')) {
-        humanScore++;
-        return playerWin;
+        (humanChoice == 'paper' && computerChoice == 'rock')) {  
+      humanScore++;
+      hmnRslt.appendChild(playerWin); 
+      return playerWin; 
     }
 
     else if ((humanChoice == 'paper' && computerChoice == 'scissors') ||
@@ -135,6 +172,25 @@ function playRound(humanChoice, computerChoice) {
         return 'Draw! Play another round'
     }
 }
+
+let result = playRound(humanChoice, computerChoice); 
+/*
+function playGame() {
+    while (humanScore < 5 && computerScore <  5) {
+        let computerChoice = getComputerChoice();
+
+        let roundResult = playRound(humanChoice, computerChoice); // storing round result from playRound() and printing to console.
+        console.log(roundResult);
+
+        console.log(`Current Score: Human ${humanScore} - Computer ${computerScore}`);
+    }
+
+    if (humanScore === 5) {
+        return `You win! Human: ${humanScore} - Computer: ${computerScore};`;
+    }
+
+    else if (computerScore === 5){
+        return `You lose! Human: ${humanScore} - Computer: ${computerScore};`;
+    }
+}
 */ 
-
-
